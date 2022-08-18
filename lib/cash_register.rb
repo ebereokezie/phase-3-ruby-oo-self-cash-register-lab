5b9b467547ca9c2b055beb0d3ec33d90371f556c
Ebere Okezie <ebereokezie@gmail.com>
2022-08-17 23:46:37 -0500

    commit tree dfa8db7512aab392792a6e31c94fd28bd0245479

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,32 @@
+class CashRegister
+    attr_accessor :discount, :total, :items, :last_transaction_price
+    def initialize(discount = "There is no discount to apply.")
+        @total = 0
+        @discount = discount
+        @items = []
+        @last_transaction_price = 0
+    end
 
+    def add_item(title, price, quantity = 1)
+    @total += price * quantity
+    quantity.times{@items << title}
+    @last_transaction_price = price * quantity
+    end
+
+    def apply_discount
+        if @discount.is_a? Integer
+             @total -= (@total * @discount.to_f/100)
+            "After the discount, the total comes to $#{@total.to_i}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def items
+        @items
+    end
+
+    def void_last_transaction
+        @total -=@last_transaction_price
+    end
+end
